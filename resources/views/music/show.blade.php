da1a0a93e1642acc10b120d5a10c631902fcc305
raul serrano <raul.serrano01@up.ac.pa>
2024-06-19 23:23:48 -0500

    intentado usar css

--- resources/views/music/show.blade.php
+++ resources/views/music/show.blade.php
@@ -3,20 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ $music->title }}</title>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body>
-    <h1>{{ $music->title }}</h1>
-    <p>Artista: {{ $music->artist }}</p>
-    <p>Álbum: {{ $music->album }}</p>
-    <p>Género: {{ $music->genre }}</p>
-    <p>Año: {{ $music->year }}</p>
-    <p>Duración: {{ $music->duration }}</p>
-    <audio controls>
-        <source src="{{ url($music->file_path) }}" type="audio/flac">
-        Tu navegador no soporta la reproducción de audio.
-    </audio>
-    <br>
-    <a href="{{ url('/musics') }}">Volver a la lista</a>
+    <div class="container">
+        <h1>{{ $music->title }}</h1>
+        <p>Artista: {{ $music->artist }}</p>
+        <p>Álbum: {{ $music->album }}</p>
+        <p>Género: {{ $music->genre }}</p>
+        <p>Año: {{ $music->year }}</p>
+        <p>Duración: {{ $music->duration }}</p>
+        <audio controls>
+            <source src="{{ url($music->file_path) }}" type="audio/flac">
+            Tu navegador no soporta la reproducción de audio.
+        </audio>
+        <br>
+        <a href="{{ url('/musics') }}">Volver a la lista</a>
+    </div>
+    <div class="fixed-player">
+        <div class="player-controls">
+            <audio controls>
+                <source src="{{ url($music->file_path) }}" type="audio/flac">
+                Tu navegador no soporta la reproducción de audio.
+            </audio>
+        </div>
+    </div>
 </body>
 </html>
-
