4af9585039f050693efa0570f775a3024e83ecd2
Raul2811 <raul.serrano01@up.ac.pa>
2024-07-10 07:19:10 -0500

    dique una mejora

--- app/Http/Controllers/MusicController.php
+++ app/Http/Controllers/MusicController.php
@@ -5,19 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Music;
 
-
 class MusicController extends Controller
 {
     public function index()
     {
-        $musics = Music::all();
+        // Usar paginación en lugar de cargar todos los registros
+        $musics = Music::paginate(10); // 10 registros por página
         return view('music.index', compact('musics'));
     }
 
     public function show($id)
     {
-        $music = Music::find($id);
+        $music = Music::findOrFail($id);
         return view('music.show', compact('music'));
     }
 }
-
